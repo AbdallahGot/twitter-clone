bc3658031e0752f46cac8a79cd581578f123c5a1
user name <abdallah897107@gmail.com>
2021-10-22 21:32:57 +0300

    update

--- script.js
+++ script.js
@@ -7,55 +7,46 @@ const tweetContent = document.getElementById('tweetContent');
 
 let tweets = [];
 let tweet = {};
-let arr =[];
+////////////////////////////////////////////////////////
 function newTweet() {
+if(userName.value.length<=250 && tweetContent.value.length<=250) {
+    let tweet = {id:tweets.length,userName:userName.value,tweetContent:tweetContent.value};
+    createTweet(tweet);
+    userName.value = "";
+    tweetContent.value ="";
+}
 
-
-const date = new Date();
+else {
+    alert('user fields shoule be less than 250 characters')
+}
+}
+function like (index){
+    document.getElementById(index).parentElement.parentElement.parentElement.classList.toggle('like');
+}
+//////////////////////////////////////////////
+///////////////////////
+function reTweet(index){
+let tweet = tweets[index];
+createTweet(tweet);
+}
+function createTweet(tweet){
+    const date = new Date();
 const hours = date.getHours();
 const minutes = date.getMinutes(); 
 const myDate = hours + ":" + minutes;
-console.log (myDate);
-
 
 let newTweet = document.createElement('div');
 newTweet.className = 'myTweet';
 newTweet.innerHTML = `<img id="myim" src="./images/avatar.jpg" alt="user">
 <div class="disc">
-    <div class="name"><h3>${userName.value} <h3> <h5 class="date"> ${myDate}</h5></div>
-    <p>${tweetContent.value}</p>
+    <div class="name"><h3>${tweet.userName} <h3> <h5 class="date"> ${myDate}</h5></div>
+    <p>${tweet.tweetContent}</p>
     <div class="tweet_icon">
-        <i class="fas fa-heart , likeBtn" id="${tweets.length}" style="color: rgb(100, 100, 100);"></i>
-        <i class="fas fa-retweet" style="color: rgb(100, 100, 100);"></i>
+        <i class="fas fa-heart , iconHov" id="${tweets.length}" onclick="like(${tweets.length})" style="color: rgb(100, 100, 100);"></i>
+        <i class="fas fa-retweet , iconHov" style="color: rgb(100, 100, 100);" onclick="reTweet(${tweets.length})"></i>
     </div>
 </div>`;
-let tweet = {id:tweets.length,userName:userName.value,tweetContent:tweetContent.value};
 tweets.push(tweet);
-tweetsContainer.appendChild(newTweet);
-console.log(tweets);
-arr = document.getElementsByClassName('likeBtn');
-
+tweetsContainer.insertBefore(newTweet,tweetsContainer.firstChild);
 }
 
-for (var i = 0; i < arr.length; i++) {
-    arr[i].addEventListener('click', cart(i)); 
- }
-function cart(){
-    let is = i.target.id;
-    console.log(is);
-}
-
-
-// arr.forEach((e)=>{
-//     e.addEventListener(click,cilii)
-// })
-// function cilii(){
-// for(let i =0 ; i<arr.length;i++){
-//     let ts = arr[i].id;
-//     tweets.forEach(e =>{
-//         if (e.id == ts) {
-//             document.getElementById('ts').parentElement.parentElement.parentElement.style.backgroundColor="blue";
-//             console.log(ts);
-//         }
-//         })
-// }}
